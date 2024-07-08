ffdf8230d33413aec1da748920361da12cb76137
Sanjay Kumar Dash <102048748+sanjay366@users.noreply.github.com>
2024-07-08 13:19:36 +0530

    Create Email.java

---
+++ Email.java
@@ -0,0 +1,8 @@
+package emailapp;
+ public class EmailApp {
+     
+    public static void main (String[] args) {
+        Email eml = new Email ("Sanjay","Dash");
+        System.out.println(eml.showInfo());
+   }
+ }
